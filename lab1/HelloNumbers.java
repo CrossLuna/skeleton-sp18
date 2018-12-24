9cdb7aaa4b4adf35b464d75ecf8f8cdbe5754456
Chao-Yang Lo <lo.chao.yang@gmail.com>
2018-12-24 12:09:02 -0500

    Fix HelloNumbers.java

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,9 +1,10 @@
 public class HelloNumbers {
     public static void main(String[] args) {
+        int sum = 0;
         int x = 0;
         while(x < 10) {
-            System.out.print(x + " ");
-            x = x + 1;
+            sum = sum + x;
+            System.out.print(sum + " ");
         }
     }
 }
